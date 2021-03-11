00fe14b25c93cd3f8e394f2cee12ef251be452a3
Richard Banks <richard.banks@tradesafe.co.za>
2021-03-11 10:16:53 +0200

    chore: remove check to diable production

--- gateway-tradesafe.php
+++ gateway-tradesafe.php
@@ -88,7 +88,7 @@ function woocommerce_tradesafe_api()
 {
     $domain = 'api-developer.tradesafe.dev';
 
-    if (get_option('tradesafe_production_mode') && true == false) {
+    if (get_option('tradesafe_production_mode')) {
         $domain = 'api.tradesafe.co.za';
     }
 
