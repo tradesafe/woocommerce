2f283587a550c6103098dc478be361a1c53ed694
Richard Banks <richard.banks@tradesafe.co.za>
2021-06-03 12:18:37 +0200

    chore: bumped version number

--- gateway-tradesafe.php
+++ gateway-tradesafe.php
@@ -5,7 +5,7 @@
  * Description: Process payments using the TradeSafe as a payments provider.
  * Author: TradeSafe
  * Author URI: https://www.tradesafe.co.za/
- * Version: 1.0.0-dev.202106011246
+ * Version: 1.0.0-dev.202106021218
  * Requires at least: 5.0
  * Tested up to: 5.7
  * WC tested up to: 5.3
